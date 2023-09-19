5732daecf3dc34159e6af3d13c41bdb21b4414cf
ITISCOOP <ITISCOOP@FIOLAPTOP296>
2023-09-19 15:49:06 -0400

    Add project files.

---
+++ ThinkRecursively/P2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int sum = 0;
+            for (int i = 1; i <= 1000; i++) {
+                Console.WriteLine($"Collect one dollar from person {i}");
+                sum += 1;
+            }
+            Console.WriteLine($"The sum is: {sum}");
+        }
+    }
+}
---
+++ ThinkRecursively/P3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 1000; //if the target%10 ==0, the program will not stop and then stack overflow.
+            int result = Collect(target);
+            Console.WriteLine($"The last result is : {result}");
+
+
+        }
+        static int Collect(int N)
+        {
+            int sum = 0;
+            if (N == 1)
+            {
+                //contribute the dollar directly              
+                sum += 1;
+                Console.WriteLine( $"Get one dollar from one person.");
+                
+            }
+            else
+            {
+                 //find 10 people
+                for (int i = 1; i <= 10; i++)
+                {
+                    //Have each collect N/10 dollars
+                    int collect = Collect(N / 10); ;
+                    //Return the money to your superior
+                    sum += collect;
+                    Console.WriteLine($"The sum is {sum}");
+                }
+                Console.WriteLine($"The superior sum is {sum}");               
+            }
+            return sum;
+        }
+    }
+}
